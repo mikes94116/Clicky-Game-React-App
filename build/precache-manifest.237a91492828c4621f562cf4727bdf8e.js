5f37b60c3c63887ab52824962a614dc91cf44b7c
mathpro2018 <mikessvk@yahoo.com>
2019-01-23 19:30:34 -0800

    updating

---
+++ build/precache-manifest.237a91492828c4621f562cf4727bdf8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e367a274b75a30a748ea",
+    "url": "/static/js/main.e367a274.chunk.js"
+  },
+  {
+    "revision": "425193f9241e750fb12a",
+    "url": "/static/js/1.425193f9.chunk.js"
+  },
+  {
+    "revision": "e367a274b75a30a748ea",
+    "url": "/static/css/main.f7582d6f.chunk.css"
+  },
+  {
+    "revision": "d06d66aa7de47521c875c8b7ebe8711c",
+    "url": "/index.html"
+  }
+];
